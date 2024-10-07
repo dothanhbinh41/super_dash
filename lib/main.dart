00ab24fcfcf0881a92ceaab365b761dd4d1be0f3
Binh Do <binhdothanh.navn@gmail.com>
2024-10-07 10:58:15 +0700

    update firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,16 @@
-import 'dart:async';
-
-import 'package:authentication_repository/authentication_repository.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'dart:async'; 
 import 'package:flutter/widgets.dart';
 import 'package:leaderboard_repository/leaderboard_repository.dart';
 import 'package:super_dash/app/app.dart';
 import 'package:super_dash/audio/audio.dart';
-import 'package:super_dash/bootstrap.dart';
-import 'package:super_dash/firebase_options_dev.dart';
+import 'package:super_dash/bootstrap.dart'; 
 import 'package:super_dash/settings/persistence/persistence.dart';
 import 'package:super_dash/settings/settings.dart';
 import 'package:super_dash/share/share.dart';
 
 final leaderboardRepository = LeaderboardRepository();
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
+  WidgetsFlutterBinding.ensureInitialized();  
   final settings = SettingsController(
     persistence: LocalStorageSettingsPersistence(),
   );
